9341d94691a6fa886ea9da731538d2ee68f5fc86
Matt Lewis <domattthings@gmail.com>
2018-08-22 15:31:04 -0700

    Add flycheck elixir stuff

--- lisp/init-elixir.el
+++ lisp/init-elixir.el
@@ -4,16 +4,24 @@
 ;;; Pretty Basic
 
 ;;; Code:
-(maybe-require-package 'elixir-mode)
+(maybe-require-package 'elixir)
 
+(require 'flycheck-elixir)
 
+;; Alchemist Stuff
 (when (maybe-require-package 'alchemist-mode)
   (after-load 'elixir
-    (add-hook 'elixir-mode-hook 'alchemist-mode))
-  (when (maybe-require-package 'company-elixir)
-    (after-load 'company
-      (add-hook 'elixir-mode-hook
-                (lambda () (sanityinc/local-push-company-backend 'company-elixir))))))
+    (add-hook 'elixir-mode-hook 'alchemist-mode)))
+
+;; Company
+(when (maybe-require-package 'company-elixir)
+  (after-load 'company
+    (add-hook 'elixir-mode-hook
+              (lambda () (sanityinc/local-push-company-backend 'company-elixir)))))
+
+;; Flycheck
+(when (maybe-require-package 'flycheck-elixir)
+  (add-hook 'elixir-mode-hook 'flycheck-mode))
 
 (provide 'init-elixir)
 ;;; init-elixir ends here
