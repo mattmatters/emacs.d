32df43a1e17b564ee37e6eb83767df0a05f5053b
Matt Lewis <domattthings@gmail.com>
2018-08-23 10:54:21 -0700

    eshell extras

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -6,5 +6,10 @@
 ;;; Code:
 (global-set-key (kbd "C-x e") 'eshell)
 
+(with-eval-after-load "esh-opt"
+  (autoload 'epe-theme-lambda "eshell-prompt-extras")
+  (setq eshell-highlight-prompt nil
+        eshell-prompt-function 'epe-theme-lambda))
+
 (provide 'init-eshell)
 ;;; init-eshell.el ends here
