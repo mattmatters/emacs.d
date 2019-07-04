53ae65a5f1c6a49392f869f05dc75ae36c1d4f8a
Matt Lewis <domattthings@gmail.com>
2019-07-04 11:26:48 -0700

    Cleanup init-elixir

--- lisp/init-elixir.el
+++ lisp/init-elixir.el
@@ -10,16 +10,14 @@
 (add-hook 'elixir-mode-hook
           (lambda () (local-set-key (kbd "C-i f") #'elixir-format)))
 
-(require 'flycheck-elixir)
+;; Flycheck
+(when (maybe-require-package 'flycheck-elixir)
+  (add-hook 'elixir-mode-hook 'flycheck-mode))
 
 ;; Alchemist Stuff
 (when (maybe-require-package 'alchemist)
   (after-load 'elixir-mode
     (add-hook 'elixir-mode-hook 'alchemist-mode)))
-
-;; Flycheck
-(when (maybe-require-package 'flycheck-elixir)
-  (add-hook 'elixir-mode-hook 'flycheck-mode))
 
 ;; Yasnippet
 (when (maybe-require-package 'yasnippet)
