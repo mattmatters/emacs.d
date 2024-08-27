dd54561a6e43991e42daabb78c054d7febcd9bd2
Matt Lewis <domattthings@gmail.com>
2024-08-27 15:38:54 -0400

    Use elixir-ts

--- lisp/init-elixir.el
+++ lisp/init-elixir.el
@@ -4,21 +4,23 @@
 ;;; Pretty Basic
 
 ;;; Code:
-(maybe-require-package 'elixir-mode)
+(maybe-require-package 'elixir-ts-mode)
 
 ;; Format kbd
-(add-hook 'elixir-mode-hook
+(add-hook 'elixir-ts-mode-hook
           (lambda () (local-set-key (kbd "C-i f") #'elixir-format)))
 
 ;; Flycheck
 (when (maybe-require-package 'flycheck-elixir)
-  (add-hook 'elixir-mode-hook 'flycheck-mode))
+  (add-hook 'elixir-ts-mode-hook 'flycheck-mode))
+
+;; Treesitter
 
 ;; Eglot
 (add-hook 'elixir-mode-hook 'eglot-ensure)
 
 (with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs '(elixir-mode "~/elixir-ls/language_server.sh")))
+  (add-to-list 'eglot-server-programs '(elixir-ts-mode "~/elixir-ls/language_server.sh")))
 
 (provide 'init-elixir)
 ;;; init-elixir ends here
