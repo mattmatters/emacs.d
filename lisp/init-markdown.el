f407b2584529aad26108f93a8f455033f3e4b303
Matt Lewis <domattthings@gmail.com>
2024-08-29 13:20:19 -0400

    Add eglot to markdown files

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -5,8 +5,11 @@
 (when (maybe-require-package 'markdown-mode)
   (add-auto-mode 'markdown-mode "\\.md\\.html\\'")
   (with-eval-after-load 'whitespace-cleanup-mode
-    (add-to-list 'whitespace-cleanup-mode-ignore-modes 'markdown-mode)))
-
+    (add-to-list 'whitespace-cleanup-mode-ignore-modes 'markdown-mode))
+  (when (executable-find "marksman")
+    (with-eval-after-load 'eglot
+      (add-to-list 'eglot-server-programs '(markdown-mode . ("marksman" "server"))))
+    (add-hook 'markdown-mode-hook #'eglot-ensure)))
 
 (provide 'init-markdown)
 ;;; init-markdown.el ends here
