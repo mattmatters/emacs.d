e7d682578ddb1fa3b876a5ada62482564c11f5c4
Matt Lewis <domattthings@gmail.com>
2024-09-09 20:34:54 -0400

    Add mjml to xml

--- lisp/init-nxml.el
+++ lisp/init-nxml.el
@@ -7,7 +7,7 @@
  (concat "\\."
          (regexp-opt
           '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss"
-            "gpx" "tcx" "plist"))
+            "gpx" "tcx" "plist" "mjml"))
          "\\'"))
 (setq magic-mode-alist (cons '("<\\?xml " . nxml-mode) magic-mode-alist))
 (fset 'xml-mode 'nxml-mode)
