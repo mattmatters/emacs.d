260addf111ddc3880b25c3174a8ceb6458550ce5
Matt Lewis <domattthings@gmail.com>
2025-05-31 13:38:05 -0400

    Use markdown mode for mdx files

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,7 +3,7 @@
 ;;; Code:
 
 (when (maybe-require-package 'markdown-mode)
-  (add-auto-mode 'markdown-mode "\\.md\\.html\\'")
+  (add-auto-mode 'markdown-mode "\\.md\\.mdx\\.html\\'")
   (with-eval-after-load 'whitespace-cleanup-mode
     (add-to-list 'whitespace-cleanup-mode-ignore-modes 'markdown-mode))
   (when (executable-find "marksman")
